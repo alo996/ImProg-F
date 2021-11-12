651036caa6a3a542ebaf408e45a814810ae9553a
Alexander Richard <richard@cip.ifi.lmu.de>
2021-11-12 19:50:05 +0100

    Declarations of four different types of tokens

--- Declarations.hs
+++ Declarations.hs
@@ -3,18 +3,46 @@
 module Declarations where
 
     data Token =
-        LogicalToken |
+        BooleanToken |
         MetaToken |
-        NameToken |
-        NumberToken
+        NameToken String |
+        NumberToken Int
 
-    -- type LogicalToken = 
-        
+    data BooleanToken = False | True deriving Show
+    
+    data MetaToken = Semicolon | 
+                     Assign | 
+                     Let | 
+                     In | 
+                     If | 
+                     Then | 
+                     Else | 
+                     LBracket | 
+                     RBracket | 
+                     And | 
+                     Not | 
+                     Or | 
+                     Equals |
+                     Plus |
+                     Minus | 
+                     Times |
+                     Divide
 
-
-        
-
-
-        
-
-
+    instance Show MetaToken where
+        show Semicolon = ";"
+        show Assign    = "="
+        show Let       = "let"
+        show In        = "in"
+        show If        = "if"
+        show Then      = "then"
+        show Else      = "else"
+        show LBracket  = "("
+        show RBracket  = ")"
+        show And       = "&"
+        show Not       = "not"
+        show Or        = "|"
+        show Equals    = "=="
+        show Plus      = "+"
+        show Minus     = "-"
+        show Times     = "*"
+        show Divide    = "/"
