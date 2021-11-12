8380857f232f48d54e649f515438db36f3a62b73
Alexander Richard <richard@cip.ifi.lmu.de>
2021-11-12 16:07:05 +0100

    Declarations module

---
+++ Declarations.hs
@@ -0,0 +1,20 @@
+-- This module contains all necessary types and aliases.
+
+module Declarations where
+
+    data Token =
+        LogicalToken |
+        MetaToken |
+        NameToken |
+        NumberToken
+
+    -- type LogicalToken = 
+        
+
+
+        
+
+
+        
+
+
