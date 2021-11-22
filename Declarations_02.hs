b7bdb6f0e04ae5b525d817e696088d172f1df236
Oz Katz <katzo@cip.ifi.lmu.de>
2021-11-22 14:56:09 +0100

    Declarations for Tokenizer_02

---
+++ Declarations_02.hs
@@ -0,0 +1,56 @@
+-- This module contains all necessary types and aliases.
+
+module Declarations
+where
+
+    data Token =
+        BooleanToken Bool |
+        KeywordToken Keyword |
+        NameToken String |
+        NumberToken Integer
+        deriving Show
+
+    data Keyword = Semicolon |
+                        Assign |
+                        Let |
+                        In |
+                        If |
+                        Then |
+                        Else |
+                        LBracket |
+                        RBracket |
+                        And |
+                        Not |
+                        Or |
+                        Equals |
+                        Greater |
+                        Less |
+                        GreaterEqu |
+                        LessEqu |
+                        Plus |
+                        Minus |
+                        Times |
+                        Divide
+
+    instance Show Keyword where
+        show Semicolon = ";"
+        show Assign    = "="
+        show Let       = "let"
+        show In        = "in"
+        show If        = "if"
+        show Then      = "then"
+        show Else      = "else"
+        show LBracket  = "("
+        show RBracket  = ")"
+        show And       = "&"
+        show Not       = "not"
+        show Or        = "|"
+        show Equals    = "=="
+        show Greater   = ">"
+        show Less      = "<"
+        show GreaterEqu= ">="
+        show LessEqu   = "<="
+        show Plus      = "+"
+        show Minus     = "-"
+        show Times     = "*"
+        show Divide    = "/"
