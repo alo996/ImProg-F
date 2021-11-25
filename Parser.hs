cd58120e0a63832dc51c42380c0a6445aa480e19
Alexander Richard <richard@cip.ifi.lmu.de>
2021-11-25 19:31:04 +0100

    First rules of grammar implemented

--- Parser.hs
+++ Parser.hs
@@ -1,11 +1,18 @@
 
-module Parser
-    (parseInterface
-    ) where
+module Parser where
 
-import Tokenizer_02
-import Data.List
-import Data.Char
+  import Tokenizer
+  import Declarations
+  import Data.List
+  import Data.Char
+
+
+--TODO-- 
+
+-- In attomicExpr the expression between "(" ")" is not defined, both as a data and the function that calls it
+-- we are not sure, that expression 8 needs to be strictly defined (per function/data) 
+--as in the EBNF in the ToyParser it is not defined that way 
+
 
 -- Grammer
 -- Programm ::= Definition ";" { Definition ";"} .
@@ -22,15 +29,59 @@ import Data.Char
 --BinärOp ::= "&" | "|" | "==" | "<" | "+" | "−" | "∗" | "/" .
 --UnärOp ::= "not" | "−" .
 --AtomarerAusdruck ::= Variable | Zahl | Wahrheitswert .
---Variable ::= Name
 
-type Parser a = [Token] -> Either (a, [SyntaxTree])
+{-
+Program ::= Definiton ";" {Definition ";"}
+Definition ::= Variable {Variable} "=" Expression
+LocalDefinitions ::= LocalDefiniton {";" LocalDefinition}
+LocalDefinition ::= Variable "=" Expression
+Expression ::= "let" LocalDefinitions "in" Expression
+| "if" Expression "then" Expression "else" Expression
+| Expression1
+Expression1 ::= Expression2 ["|" Expression1 ]
+Expression2 ::= Expression3 ["&" Expression2 ]
+Expression3 ::= ["not"] Expression4
+Expression4 ::= Expression5 [ComparisonOperator Expression5 ]
+Expression5 ::= Expression6 RestExpression5
+RestExpression5 ::= {"+" Expression6 } | "-" Expression6
+Expression6 ::= ["-"] Expression7
+Expression7 ::= Expression8 RestExpression7
+RestExpression7 ::= {"*" Expression8 } | "/" Expression8
+Expression8 ::= AtomicExpression {AtomicExpression}
+AtomicExpression ::= Variable | Literal | "(" Expression ")"
+ComparisonOperator ::= "==" | "<"
+Variable ::= Name
+-}
 
-parse :: [(Token, Int)] -> SyntaxTree
 
-variable :: Parser Expression
-variable (Token x : _ ) = do
-  case x of
-    NameToken : _ -> return (Name x, _)
-    _ -> Nothing
-    variable _ = Nothing
+  --newtype Parser a = P ([Token] -> (Either String a, [Token]))
+  type Parser a = [Token] -> (Either String a, [Token])
+
+  data Expression = Expression String | Name String | BoolLit Bool | NumLit Int
+
+-- parse :: [(Token, Int)] -> SyntaxTree
+
+-- [(NameToken "f",1),(NameToken "x",1),(KeywordToken =,1),(NumberToken 3,1)]
+
+  variable, atomicExpression :: Parser Expression
+  variable (NameToken x : xs) = (Right (Name x), xs)
+  variable (_ : xs)           = (Left "we do not understand yet", xs)
+
+
+
+  atomicExpression (NameToken x : xs)           = variable (NameToken x : xs)
+  atomicExpression (BooleanToken x : xs)        = (Right (BoolLit x), xs)
+  atomicExpression (NumberToken x : xs)         = (Right (NumLit x), xs)
+  --atomicExpression (KeywordToken LBracket : xs) = do 
+  --            (e, ys) <- expression xs
+  --            case ys of
+  --                (KeywordToken RBracket : zs) -> (Right e, zs)
+  --                (_ : zs)                     -> (Left "what is happening here hahah", zs)  
+  atomicExpression  (_ : xs)                = (Left "we do not understand yet", xs)
+
+
+  expression :: Parser Expression
+  expression = undefined
+
+  restExpr7 :: Parser [Expression]
+  restExpr7 = undefined
