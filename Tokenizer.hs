084c0aab8ba7ac1fc17b8f4591bac0ac3cd009e7
Alexander Richard <richard@cip.ifi.lmu.de>
2021-11-26 16:16:55 +0100

    Added comments

--- Tokenizer.hs
+++ Tokenizer.hs
@@ -5,17 +5,16 @@ module Tokenizer where
     import Data.Char
     import Declarations
     
-    -- 'tokenize' receives user input and transforms it to a list of tuples, each containing a token its respective line in the source code
+    -- tokenize receives user input and transforms it to a list of tuples, each containing a token its respective line in the source code
     tokenize :: String -> [(Token, Int)]
     tokenize "" = error "Can not compile empty program."
     tokenize program = tokenize' program [] 1
 
-    -- helper method that recursively iterates through the user input and returns the intended output specified in 'tokenize'
+    -- tokenize' deals with spaces and tokenizes keywords and booleans
     tokenize' :: String -> [(Token, Int)] -> Int -> [(Token, Int)]
-    -- base case
     tokenize' [] tokenAcc lineAcc = reverse tokenAcc
 
-    -- next character is a whitespace or newline
+    -- next character is a space
     tokenize' (' ' : xs) tokenAcc lineAcc         = tokenize' xs tokenAcc lineAcc
     tokenize' ('\t' : xs) tokenAcc lineAcc        = tokenize' xs tokenAcc lineAcc
     tokenize' ('\r' : '\n' : xs) tokenAcc lineAcc = tokenize' xs tokenAcc (lineAcc + 1)
@@ -23,15 +22,15 @@ module Tokenizer where
     tokenize' ('\r' : xs) tokenAcc lineAcc        = tokenize' xs tokenAcc (lineAcc + 1)
 
     -- next characters form a multiple character keyword or a boolean value
-    tokenize' ('=' : '=' : xs) tokenAcc lineAcc                         = tokenize' xs ((KeywordToken Equals , lineAcc) : tokenAcc) lineAcc
-    tokenize' ('e' : 'l' : 's' : 'e' : xs) tokenAcc lineAcc             = tokenize' xs ((KeywordToken Else , lineAcc) : tokenAcc) lineAcc
-    tokenize' ('f' : 'a' : 'l' : 's' : 'e' : xs) tokenAcc lineAcc       = tokenize' xs ((BooleanToken $ BoolF False, lineAcc) : tokenAcc) lineAcc
-    tokenize' ('i' : 'f' : xs) tokenAcc lineAcc                         = tokenize' xs ((KeywordToken If , lineAcc) : tokenAcc) lineAcc
-    tokenize' ('i' : 'n' : xs) tokenAcc lineAcc                         = tokenize' xs ((KeywordToken In , lineAcc) : tokenAcc) lineAcc
-    tokenize' ('l' : 'e' : 't' : xs) tokenAcc lineAcc                   = tokenize' xs ((KeywordToken Let , lineAcc) : tokenAcc) lineAcc
-    tokenize' ('n' : 'o' : 't' : xs) tokenAcc lineAcc                   = tokenize' xs ((KeywordToken Not , lineAcc) : tokenAcc) lineAcc
-    tokenize' ('t' : 'h' : 'e' : 'n' : xs) tokenAcc lineAcc             = tokenize' xs ((KeywordToken Then , lineAcc) : tokenAcc) lineAcc
-    tokenize' ('t' : 'r' : 'u' : 'e' : xs) tokenAcc lineAcc             = tokenize' xs ((BooleanToken $ BoolF True, lineAcc) : tokenAcc) lineAcc
+    tokenize' ('=' : '=' : xs) tokenAcc lineAcc                   = tokenize' xs ((KeywordToken Equals , lineAcc) : tokenAcc) lineAcc
+    tokenize' ('e' : 'l' : 's' : 'e' : xs) tokenAcc lineAcc       = tokenize' xs ((KeywordToken Else , lineAcc) : tokenAcc) lineAcc
+    tokenize' ('f' : 'a' : 'l' : 's' : 'e' : xs) tokenAcc lineAcc = tokenize' xs ((BooleanToken $ BoolF False, lineAcc) : tokenAcc) lineAcc
+    tokenize' ('i' : 'f' : xs) tokenAcc lineAcc                   = tokenize' xs ((KeywordToken If , lineAcc) : tokenAcc) lineAcc
+    tokenize' ('i' : 'n' : xs) tokenAcc lineAcc                   = tokenize' xs ((KeywordToken In , lineAcc) : tokenAcc) lineAcc
+    tokenize' ('l' : 'e' : 't' : xs) tokenAcc lineAcc             = tokenize' xs ((KeywordToken Let , lineAcc) : tokenAcc) lineAcc
+    tokenize' ('n' : 'o' : 't' : xs) tokenAcc lineAcc             = tokenize' xs ((KeywordToken Not , lineAcc) : tokenAcc) lineAcc
+    tokenize' ('t' : 'h' : 'e' : 'n' : xs) tokenAcc lineAcc       = tokenize' xs ((KeywordToken Then , lineAcc) : tokenAcc) lineAcc
+    tokenize' ('t' : 'r' : 'u' : 'e' : xs) tokenAcc lineAcc       = tokenize' xs ((BooleanToken $ BoolF True, lineAcc) : tokenAcc) lineAcc
 
     -- next character forms a keyword
     tokenize' (x:xs) tokenAcc lineAcc = case x of
