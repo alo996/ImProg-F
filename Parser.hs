b34d84955cd6fdc7534d03dd84ab934d08c80c9e
Alexander Richard <richard@cip.ifi.lmu.de>
2021-12-01 16:23:01 +0100

    blabla

--- Parser.hs
+++ Parser.hs
@@ -1,27 +1,27 @@
 module Parser where
 
-  import Tokenizer
+  import Tokenizer_is
   import Declarations
   import Data.List
   import Data.Char
-
--- Grammar
--- Programm ::= Definition ";" { Definition ";"} .
---Definition ::= Variable {Variable} "=" Ausdruck .
---Lokaldefinitionen ::= Lokaldefinition { ";" Lokaldefinition } .
---Lokaldefinition ::= Variable "=" Ausdruck .
---Ausdruck ::= "let" Lokaldefinitionen "in" Ausdruck
--- | "if" Ausdruck "then" Ausdruck "else" Ausdruck
--- | Ausdruck BinärOp Ausdruck
--- | UnärOp Ausdruck
--- | Ausdruck Ausdruck
--- | "(" Ausdruck ")"
--- | AtomarerAusdruck .
---BinärOp ::= "&" | "|" | "==" | "<" | "+" | "−" | "∗" | "/" .
---UnärOp ::= "not" | "−" .
---AtomarerAusdruck ::= Variable | Zahl | Wahrheitswert .
-
+  
 {-
+Grammar
+Programm ::= Definition ";" { Definition ";"} .
+Definition ::= Variable {Variable} "=" Ausdruck .
+Lokaldefinitionen ::= Lokaldefinition { ";" Lokaldefinition } .
+Lokaldefinition ::= Variable "=" Ausdruck .
+Ausdruck ::= "let" Lokaldefinitionen "in" Ausdruck
+ | "if" Ausdruck "then" Ausdruck "else" Ausdruck
+ | Ausdruck BinärOp Ausdruck
+ | UnärOp Ausdruck
+ | Ausdruck Ausdruck
+ | "(" Ausdruck ")"
+ | AtomarerAusdruck .
+BinärOp ::= "&" | "|" | "==" | "<" | "+" | "−" | "∗" | "/" .
+UnärOp ::= "not" | "−" .
+AtomarerAusdruck ::= Variable | Zahl | Wahrheitswert .
+
 Program ::= Definiton ";" {Definition ";"}
 Definition ::= Variable {Variable} "=" Expression
 LocalDefinitions ::= LocalDefiniton {";" LocalDefinition}
@@ -44,52 +44,66 @@ ComparisonOperator ::= "==" | "<"
 Variable ::= Name
 -}
 
+  type Parser a = [Token] -> (Either String a, [Token])
 
-type Parser a = [Token] -> (Either String a, [Token])
+  --data Expression = Expression String | Name String | BoolLit Bool | NumLit Int | Mult atomExpression atomExpression 
 
---data Expression = Expression String | Name String | BoolLit Bool | NumLit Int | Mult atomExpression atomExpression 
+  data Expression = MulDiv AtomicExpr AtomicExpr | UnOp Expression | AtomicExpr
+  
+  data AtomicExpr = Name String | LitBool Bool | LitNum Int deriving Show
 
- data Expr = MulDiv AtomExpr Expr | UnOp KeywordToken Expression
+  -- [(NameToken "f",1),(NameToken "x",1),(KeywordToken =,1),(NumberToken 3,1)]
 
--- data AtomicExpr = Var String | LitBool Bool | LitNum Int | Expr Expr 
-
--- [(NameToken "f",1),(NameToken "x",1),(KeywordToken =,1),(NumberToken 3,1)]
-
-  variable, atomicExpression :: Parser Expression
-  variable (NameToken x : xs) = (Right (Name x), xs)
-  variable (_ : xs)           = (Left "we do not understand yet", xs)
-
-  atomicExpression (NameToken x : xs)           = variable (NameToken x : xs)
-  atomicExpression (BooleanToken x : xs)        = (Right (BoolLit x), xs)
-  atomicExpression (NumberToken x : xs)         = (Right (NumLit x), xs)
+  variable :: [(Token, Int)] -> (Either String AtomicExpr, [(Token, Int)])
+  variable ((NameToken x, lc) : xs)       = (Right (Name x), xs)
+  variable ((_, lc) : xs)                 = (Left ("Parse error in line " ++ show lc), xs)
+  
+  atomicExpression :: [(Token, Int)] -> (Either String AtomicExpr, [(Token, Int)])
+  atomicExpression a@((NameToken x, lc) : xs)        = variable a
+  atomicExpression ((BooleanToken x : xs), lc)       = (Right (BoolLit x), xs)
+  atomicExpression ((NumberToken x : xs), lc)        = (Right (NumLit x), xs)
   -- atomicExpression (KeywordToken LBracket : xs) = do 
   --            (e, ys) <- expression xs
   --            case ys of
   --                (KeywordToken RBracket : zs) -> (Right e, zs)
   --                (_ : zs)                     -> (Left "what is happening here hahah", zs)  
-  atomicExpression  (_ : xs)                    = (Left "we do not understand yet", xs)
+  atomicExpression  ((_, lc): xs)                    = (Left ("Parse error in line " ++ show lc), xs)
 
-  expr8 :: Parser [Expression]
+{-
+
+  expr8 :: [Token] -> (Either String [Expression], [Token])
   expr8 xs = do
     (e, ys) <- atomicExpression xs
     (es, zs) <- expr8 ys
     return (e : es, zs)
     
-  restExpr7 :: Parser [Expression]
+  restExpr7 :: [Token] -> (Either String [Expression], [Token])
   restExpr7 (KeywordToken Times : xs) = do
-    (e, ys) <- atomicExpression xs
-    (es, zs) <- restExpr ys
+    (e, ys) <- expr8 xs
+    (es, zs) <- restExpr7 ys
     return (e : es, zs)
-  restExpr7 (KeywordToken Divide : xs) = undefined
+  restExpr7 (KeywordToken Divide : xs) = do
+    (e, ys) <- expr8 xs
+    return (e, ys)
 
-  expr7 :: Parser [Expression]
+  expr7 :: [Token] -> (Either String Expression, [Token])
   expr7 xs = do
     (e, ys) <- expr8 xs
     (es, zs) <- restExpr7 ys
     return (foldl MulDiv e es, zs)
+    --maybe to solve the MulDiv with IF THEN ElSE
 
-  expr6 :: Parser Expression
-  expr6 (KeywordToken Not : xs) = expr7 
+  expr6 :: [Token] -> (Either String Expression, [Token])
+  expr6 (KeywordToken Minus : xs) = do
+    (UnOp, ys) <- expr7 xs
+    return (UnOp, ys)
+  expr6 (_ : xs)                  = expr7 xs
 
-  expression :: Parser Expression
-  expression = undefined
+  
+-}
+  
+
+
+
+  --expression :: Parser Expression
+  --expression = undefined
