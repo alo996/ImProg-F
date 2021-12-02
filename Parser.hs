4ceb2a0b234bbebb0868d9f1c11e33346c1b2767
Alexander Richard <richard@cip.ifi.lmu.de>
2021-12-02 18:38:10 +0100

    Implemented a function for each non-terminal

--- Parser.hs
+++ Parser.hs
@@ -1,10 +1,10 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Parser where
 
   import Tokenizer_is
   import Declarations
   import Data.List
   import Data.Char
-  import Distribution.SPDX (LicenseId(XSkat))
   
 {-
 Grammar
@@ -33,7 +33,7 @@ Expression ::= "let" LocalDefinitions "in" Expression
 Expression1 ::= Expression2 ["|" Expression1 ]
 Expression2 ::= Expression3 ["&" Expression2 ]
 Expression3 ::= ["not"] Expression4
-Expression4 ::= Expression5 [ComparisonOperator Expression5 ]
+Expression4 ::= Expression5 [ComparisonOperator Expression5]
 Expression5 ::= Expression6 RestExpression5
 RestExpression5 ::= {"+" Expression6 } | "-" Expression6
 Expression6 ::= ["-"] Expression7
@@ -43,64 +43,166 @@ Expression8 ::= AtomicExpression {AtomicExpression}
 AtomicExpression ::= Variable | Literal | "(" Expression ")"
 ComparisonOperator ::= "==" | "<"
 Variable ::= Name
+
+data Expression = Expression String | Name String | BoolLit Bool | NumLit Int | Mult atomExpression atomExpression 
 -}
 
   type Parser a = [(Token, Int)] -> Either String (a, [(Token, Int)])
 
-  --data Expression = Expression String | Name String | BoolLit Bool | NumLit Int | Mult atomExpression atomExpression 
+  data Expression 
+          = Add Expression Expression 
+          | Mult Expression Expression
+          | Div Expression Expression
+          | BinaryMin Expression Expression
+          | UnaryMin Expression
+          | Equal Expression Expression
+          | LessThan Expression Expression
+          | LogicalAnd Expression Expression
+          | LogicalOr Expression Expression
+          | LogicalNot Expression
+          | LetIn LocalDefinitions Expression
+          | IfThenElse Expression Expression Expression
+          | AtomicExpr AtomicExpression
 
-  data Expression = MulDiv AtomicExpr AtomicExpr | UnOp Expression | AtomicExpr
+  data LocalDefinition = Assignment Variable Expression
+  type LocalDefinitions = [LocalDefinition]
+
+  type Variable = String
+  data AtomicExpression = Var Variable | LitBool BoolF | LitNum Int | Expr Expression
+
+
+  variable :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
+  variable ((NameToken name, lc) : xs) = Right ([AtomicExpr (Var name)], xs)
+  variable ((x , lc) : xs)             = Left $ "Parse error on input " ++ show x ++ " in line " ++ show lc
+  variable []                          = Left "Parse error"
   
-  data AtomicExpr = Name String | LitBool BoolF | LitNum Int deriving Show
+  atomicExpression :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
+  atomicExpression a@((NameToken x, lc) : xs)                  = variable a
+  atomicExpression ((BooleanToken b@(BoolF x), lc) : xs)       = Right ([AtomicExpr (LitBool (BoolF x))], xs)
+  atomicExpression ((NumberToken x, lc) : xs)                  = Right ([AtomicExpr (LitNum x)], xs)
+  atomicExpression ((KeywordToken LBracket, lc) : xs)          = do 
+    (e, ys) <- expr xs
+    case ys of
+      ((KeywordToken RBracket, lc) : zs) -> return (e, ys)
+      (_ : zs)                           -> Left "Parse error"
+  atomicExpression ((_, lc): xs)                               = Left $ "Parse error in line " ++ show lc
 
+  expr8 :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
+  expr8 xs = do
+    (e, ys) <- atomicExpression xs
+    (es, zs) <- expr8 ys
+    return (e ++ es, zs)
 
-  -- [(NameToken "f",1),(NameToken "x",1),(KeywordToken =,1),(NumberToken 3,1)]
-  
-  atomicExpression :: [(Token, Int)] -> Either String (AtomicExpr, [(Token, Int)])
-  atomicExpression ((NameToken x, lc) : xs)                  = Right (Name x, xs)
-  atomicExpression ((BooleanToken b@(BoolF x), lc) : xs)     = Right (LitBool b, xs)
-  atomicExpression ((NumberToken x, lc) : xs)                = Right (LitNum x, xs)
-  --atomicExpression ((KeywordToken LBracket, lc) : xs)      = do 
-  --              (e, ys) <- expression xs
---              case ys of
---                  (KeywordToken RBracket : zs) -> (Right e, zs)
---                  (_ : zs)                     -> (Left "what is happening here hahah", zs)  
-  atomicExpression ((_, lc): xs)                             = Left $ "Parse error in line " ++ show lc   -- it dosent work for now
-
-
-  expr8 :: [(Token, Int)] -> Either String ([AtomicExpr], [(Token, Int)])
-  expr8 a@(x : xs) = do
-    (expr, xs) <- atomicExpression a
-    (exprs, ys) <- expr8 xs
-    return ((expr : exprs), ys)
-
-{-
-  restExpr7 :: [Token] -> (Either String [Expression], [Token])
-  restExpr7 (KeywordToken Times : xs) = do
+  restExpr7 :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
+  restExpr7 ((KeywordToken Times, _) : xs) = do
     (e, ys) <- expr8 xs
     (es, zs) <- restExpr7 ys
-    return (e : es, zs)
-  restExpr7 (KeywordToken Divide : xs) = do
+    return (e ++ es, zs)
+  restExpr7 ((KeywordToken Divide, _) : xs) = do
     (e, ys) <- expr8 xs
     return (e, ys)
 
-  expr7 :: [Token] -> (Either String Expression, [Token])
+  expr7 :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
   expr7 xs = do
     (e, ys) <- expr8 xs
     (es, zs) <- restExpr7 ys
-    return (foldl MulDiv e es, zs)
-    --maybe to solve the MulDiv with IF THEN ElSE
+    return (e ++ es, zs)
 
-  expr6 :: [Token] -> (Either String Expression, [Token])
-  expr6 (KeywordToken Minus : xs) = do
-    (UnOp, ys) <- expr7 xs
-    return (UnOp, ys)
-  expr6 (_ : xs)                  = expr7 xs
+  expr6 :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
+  expr6 ((KeywordToken Minus, _) : xs) = do
+    (e, ys) <- expr7 xs
+    return (e, ys)
+  expr6 (_ : xs)                       = expr7 xs
   
+  restExpr5 :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
+  restExpr5 ((KeywordToken Plus, _) : xs) = do
+    (e, ys) <- expr6 xs
+    (es, zs) <- restExpr5 ys
+    return (e ++ es, zs)
+  restExpr5 ((KeywordToken Minus, _) : xs) = expr6 xs
 
+  expr5 :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
+  expr5 xs = do
+    (e, ys) <- expr6 xs
+    (es, zs) <- restExpr5 ys
+    return (e ++ es, zs)
 
+  expr4 :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
+  expr4 xs = do
+    (e, ys) <- expr5 xs
+    case ys of 
+      ((KeywordToken Equals, lc) : zs) -> do
+        (es, as) <- expr5 zs
+        return (e ++ es, as)
+      ((KeywordToken Less, lc) : zs) -> do
+        (es, as) <- expr5 zs
+        return (e ++ es, as)
+      _ -> return (e, ys)
 
-  expression :: Parser Expression
-  expression = undefined
+  expr3 :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
+  expr3 ((KeywordToken Not, _) : xs) = expr4 xs
+  expr3 ((_, _) : xs)                = expr4 xs
 
-  -}
+  expr2 :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
+  expr2 xs = do
+    (e, ys) <- expr3 xs
+    case ys of 
+      ((KeywordToken And, lc) : zs) -> do
+        (es, as) <- expr2 zs
+        return (e ++ es, as)
+      _ -> return (e, ys)
+
+  expr1 :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
+  expr1 xs = do
+    (e, ys) <- expr2 xs
+    case ys of 
+      ((KeywordToken Or, lc) : zs) -> do
+        (es, as) <- expr1 zs
+        return (e ++ es, as)
+      _ -> return (e, ys)
+
+  expr :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
+  expr ((KeywordToken Let, lc) : xs) = do
+    (e, ys) <- localDefs xs
+    case ys of
+      ((KeywordToken In, lc) : zs) -> expr zs
+      _                            -> Left "Parse error"
+  expr ((KeywordToken If, lc) : xs) = do
+    (e, ys) <- expr xs
+    case ys of
+      ((KeywordToken Then, lc) : zs) -> do
+        (f, as) <- expr zs
+        case as of 
+          ((KeywordToken Else, lc) : as) -> expr as
+          _                              -> Left "Parse error"
+      _                              -> Left "Parse error"
+  expr xs                           = expr1 xs
+
+  localDef :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
+  localDef ((NameToken name, lc) : xs) = do
+    (e, ys) <- variable [(NameToken name, lc)]
+    case ys of
+      ((KeywordToken Assign, lc) : zs) -> expr zs
+      _                                -> Left "Parse error"
+
+  localDefs :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
+  localDefs xs = do
+    (e, ys) <- localDef xs
+    case ys of 
+      ((KeywordToken Semicolon, lc) : zs) -> localDefs zs
+      _                                   -> return (e, ys)
+
+  def :: [(Token, Int)] -> Either String ([Expression], [(Token, Int)])
+  def xs = do
+    (e, ys) <- variable xs
+    case ys of
+      ((NameToken name1, lc) : zs) -> def zs
+      ((KeywordToken Assign, lc) : zs) -> expr zs
+      _                                -> Left "Parse error"
+
+  program :: [(Token, Int)] -> Either String (Expression, [(Token, Int)])
+  program xs = do
+    (e, ys) <- def xs
+    case ys of
+      ((KeywordToken Semicolon, lc) : zs) -> program zs
+      _                                   -> Left "Parse error"
