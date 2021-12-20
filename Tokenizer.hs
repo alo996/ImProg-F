f5764254d5b55cc3535ae24f07e8f987e6388662
Alexander Richard <richard@cip.ifi.lmu.de>
2021-12-20 12:07:47 +0100

    Improved readability, added comments and did tests.

--- Tokenizer.hs
+++ Tokenizer.hs
@@ -1,72 +1,36 @@
 -- This module contains all functionality to pursue lexical analysis in F.
-
-module Tokenizer_is where
-
-    {-
-    if3==3 vs if3 x=2
-    truefalse vs true3false
-    true=false
-
-    IF : muss alleine stehen oder ohne Leerzeichen folgt Bedingung in Klammern
-
-    either a keyword made up of letters (beispiel: if else etc.)
-    is directly followed by a whitespace OR
-    a keyword not made up of letters (beispiel: + = - etc.) -> then it is accepted as KeywordToken
-
-    true/false:
-    - if true==false ; should be tokenized as (if)(true)(==)(false)
-    - if(true==false) ; should be tokenized as (if)(()(true)(==)(false)())
-    - let true3 = false3 ; should be tokenized as (let)(true3)(=)(false3)
-    - truefalse x = 1 ; should be tokenized as (truefalse)(x)(=)(1)
-    if/then/else:
-    - if3 x = 3 ;
-    - ifthenelse x = 3;
-    - if(3==3)then(2)else(3) ;
-    - if true then3else4 ;
-    - if true then true else false ;
-    let/in:
-    - let3 ; NameToken
-    - in3 ; NameToken
-    - let(x=3)in(x*x) ; should be tokenized as (let)(()(x)(=)(3)(in)(()(x)(*)(x)
-
-
-    tokenize "iffalse"
-    evtl 32a fall gleich im Tokenizer abfangen
-    evtl main zu keyword?
-    truefalseletthen ok, true=false ok
-    -}
-
-    import Data.Char
+module Tokenizer where
+    import Data.Char ( isAlphaNum, isDigit )
     import Declarations
 
-    -- tokenize receives user input and transforms it to a list of tuples, each containing a token its respective line in the source code
+    -- tokenize receives user input and transforms it to a list of tuples, each containing a token and its respective line in the source code.
     tokenize :: String -> [(Token, Int)]
-    tokenize "" = error "Can not compile empty program."
+    tokenize ""      = error "Can not compile empty program."
     tokenize program = tokenize' program [] 1
 
-    -- tokenize' deals with spaces and tokenizes keywords and booleans
+    -- tokenize' deals with spaces and tokenizes keywords and booleans.
     tokenize' :: String -> [(Token, Int)] -> Int -> [(Token, Int)]
     tokenize' [] tokenAcc lineAcc = reverse tokenAcc
 
-    -- next character is a space
+    -- Next character is a space.
     tokenize' (' ' : xs) tokenAcc lineAcc         = tokenize' xs tokenAcc lineAcc
     tokenize' ('\t' : xs) tokenAcc lineAcc        = tokenize' xs tokenAcc lineAcc
     tokenize' ('\r' : '\n' : xs) tokenAcc lineAcc = tokenize' xs tokenAcc (lineAcc + 1)
     tokenize' ('\n' : xs) tokenAcc lineAcc        = tokenize' xs tokenAcc (lineAcc + 1)
     tokenize' ('\r' : xs) tokenAcc lineAcc        = tokenize' xs tokenAcc (lineAcc + 1)
 
-    -- next characters form a multiple character keyword or a boolean value & only if followed by symbol or space
-    tokenize' ('=' : '=' : xs) tokenAcc lineAcc                       = tokenize' xs ((KeywordToken Equals , lineAcc) : tokenAcc) lineAcc
-    tokenize' ('e' : 'l' : 's' : 'e' : a : xs) tokenAcc lineAcc       = if (validateChar a) then tokenize' (a:xs) ((KeywordToken Else , lineAcc) : tokenAcc) lineAcc else tokenizeNames ('e' : 'l' : 's' : 'e' : a : xs) tokenAcc lineAcc ""
-    tokenize' ('f' : 'a' : 'l' : 's' : 'e' : a : xs) tokenAcc lineAcc = if (validateChar a) then tokenize' (a:xs) ((BooleanToken $ BoolF False, lineAcc) : tokenAcc) lineAcc else tokenizeNames ('f' : 'a' : 'l' : 's' : 'e' : a : xs) tokenAcc lineAcc ""
-    tokenize' ('i' : 'f' : a : xs) tokenAcc lineAcc                   = if (validateChar a) then tokenize' (a:xs) ((KeywordToken If , lineAcc) : tokenAcc) lineAcc else tokenizeNames ('i' : 'f' : a : xs) tokenAcc lineAcc ""
-    tokenize' ('i' : 'n' : a : xs) tokenAcc lineAcc                   = if (validateChar a) then tokenize' (a:xs) ((KeywordToken In , lineAcc) : tokenAcc) lineAcc else tokenizeNames ('i' : 'n' : a : xs) tokenAcc lineAcc ""
-    tokenize' ('l' : 'e' : 't' : a : xs) tokenAcc lineAcc             = if (validateChar a) then tokenize' (a:xs) ((KeywordToken Let , lineAcc) : tokenAcc) lineAcc else tokenizeNames ('l' : 'e' : 't' : a : xs) tokenAcc lineAcc ""
-    tokenize' ('n' : 'o' : 't' : a : xs) tokenAcc lineAcc             = if (validateChar a) then tokenize' (a:xs) ((KeywordToken Not , lineAcc) : tokenAcc) lineAcc else tokenizeNames ('n' : 'o' : 't' : a : xs) tokenAcc lineAcc ""
-    tokenize' ('t' : 'h' : 'e' : 'n' : a : xs) tokenAcc lineAcc       = if (validateChar a) then tokenize' (a:xs) ((KeywordToken Then , lineAcc) : tokenAcc) lineAcc else tokenizeNames ('t' : 'h' : 'e' : 'n' : a : xs) tokenAcc lineAcc ""
-    tokenize' ('t' : 'r' : 'u' : 'e' : a : xs) tokenAcc lineAcc       = if (validateChar a) then tokenize' (a:xs) ((BooleanToken $ BoolF True, lineAcc) : tokenAcc) lineAcc else tokenizeNames ('t' : 'r' : 'u' : 'e' : a : xs) tokenAcc lineAcc ""
+    -- Next characters form a multiple character keyword or a boolean value, followed by a reserved special character or space.
+    tokenize' token@('=' : '=' : xs) tokenAcc lineAcc                       = tokenize' xs ((KeywordToken Equals , lineAcc) : tokenAcc) lineAcc
+    tokenize' token@('e' : 'l' : 's' : 'e' : x : xs) tokenAcc lineAcc       = if validateChar x then tokenize' (x : xs) ((KeywordToken Else , lineAcc) : tokenAcc) lineAcc         else tokenizeNames token tokenAcc lineAcc ""
+    tokenize' token@('f' : 'a' : 'l' : 's' : 'e' : x : xs) tokenAcc lineAcc = if validateChar x then tokenize' (x : xs) ((BooleanToken $ BoolF False, lineAcc) : tokenAcc) lineAcc else tokenizeNames token tokenAcc lineAcc ""
+    tokenize' token@('i' : 'f' : x : xs) tokenAcc lineAcc                   = if validateChar x then tokenize' (x : xs) ((KeywordToken If , lineAcc) : tokenAcc) lineAcc           else tokenizeNames token tokenAcc lineAcc ""
+    tokenize' token@('i' : 'n' : x : xs) tokenAcc lineAcc                   = if validateChar x then tokenize' (x : xs) ((KeywordToken In , lineAcc) : tokenAcc) lineAcc           else tokenizeNames token tokenAcc lineAcc ""
+    tokenize' token@('l' : 'e' : 't' : x : xs) tokenAcc lineAcc             = if validateChar x then tokenize' (x : xs) ((KeywordToken Let , lineAcc) : tokenAcc) lineAcc          else tokenizeNames token tokenAcc lineAcc ""
+    tokenize' token@('n' : 'o' : 't' : x : xs) tokenAcc lineAcc             = if validateChar x then tokenize' (x : xs) ((KeywordToken Not , lineAcc) : tokenAcc) lineAcc          else tokenizeNames token tokenAcc lineAcc ""
+    tokenize' token@('t' : 'h' : 'e' : 'n' : x : xs) tokenAcc lineAcc       = if validateChar x then tokenize' (x : xs) ((KeywordToken Then , lineAcc) : tokenAcc) lineAcc         else tokenizeNames token tokenAcc lineAcc ""
+    tokenize' token@('t' : 'r' : 'u' : 'e' : x : xs) tokenAcc lineAcc       = if validateChar x then tokenize' (x : xs) ((BooleanToken $ BoolF True, lineAcc) : tokenAcc) lineAcc  else tokenizeNames token tokenAcc lineAcc ""
 
-    -- if keyword is at the end
+    -- If multiple character keyword is either at the end of ... or the program.
     tokenize' ('e' : 'l' : 's' : 'e' : xs) tokenAcc lineAcc           = tokenize' xs ((KeywordToken Else , lineAcc) : tokenAcc) lineAcc
     tokenize' ('f' : 'a' : 'l' : 's' : 'e' : xs) tokenAcc lineAcc     = tokenize' xs ((BooleanToken $ BoolF False , lineAcc) : tokenAcc) lineAcc
     tokenize' ('i' : 'f' : xs) tokenAcc lineAcc                       = tokenize' xs ((KeywordToken If , lineAcc) : tokenAcc) lineAcc
@@ -76,8 +40,8 @@ module Tokenizer_is where
     tokenize' ('t' : 'h' : 'e' : 'n' : xs) tokenAcc lineAcc           = tokenize' xs ((KeywordToken Then , lineAcc) : tokenAcc) lineAcc
     tokenize' ('t' : 'r' : 'u' : 'e' : xs) tokenAcc lineAcc           = tokenize' xs ((BooleanToken $ BoolF True , lineAcc) : tokenAcc) lineAcc
 
-    -- next character forms a keyword
-    tokenize' (x:xs) tokenAcc lineAcc = case x of
+    -- Next character is a single character keyword.
+    tokenize' (x : xs) tokenAcc lineAcc = case x of
         '&' -> tokenize' xs ((KeywordToken And , lineAcc) : tokenAcc) lineAcc
         '=' -> tokenize' xs ((KeywordToken Assign , lineAcc) : tokenAcc) lineAcc
         '/' -> tokenize' xs ((KeywordToken Divide , lineAcc) : tokenAcc) lineAcc
@@ -93,21 +57,22 @@ module Tokenizer_is where
                     then if isDigit x
                         then tokenizeNumbers (x:xs) tokenAcc lineAcc ""
                         else tokenizeNames (x:xs) tokenAcc lineAcc ""
-                    else error $ "Syntax error: illegal character on input " ++ show x ++ " in line " ++ show lineAcc
+                    else error $ "Syntax error: illegal character on input " ++ show x ++ " in line " ++ show lineAcc ++ "."
 
-    -- tokenizes numbers or names
+    -- Tokenize integers or identifiers.
     tokenizeNumbers, tokenizeNames :: String -> [(Token, Int)] -> Int -> String -> [(Token, Int)]
     tokenizeNumbers [x] tokenAcc lineAcc number            = reverse $ (NumberToken (read (number ++ [x]) :: Int), lineAcc) : tokenAcc
     tokenizeNumbers (x : y : xs) tokenAcc lineAcc number   = if isDigit y
                                                                 then tokenizeNumbers (y : xs) tokenAcc lineAcc (number ++ [x])
                                                                 else tokenize' (y : xs) ((NumberToken (read (number ++ [x]) :: Int), lineAcc) : tokenAcc) lineAcc
-    tokenizeNumbers _ _ _ _                                = undefined
+    tokenizeNumbers _ _ _ _                                = undefined -- This case can never be reached.
 
     tokenizeNames [x] tokenAcc lineAcc name                = reverse $ (NameToken $ name ++ [x], lineAcc) : tokenAcc
     tokenizeNames (x : y : xs) tokenAcc lineAcc name       = if isAlphaNum y
                                                                 then tokenizeNames (y : xs) tokenAcc lineAcc (name ++ [x])
                                                                 else tokenize' (y : xs) ((NameToken $ name ++ [x], lineAcc) : tokenAcc) lineAcc
-    tokenizeNames _ _ _ _                                  = undefined
+    tokenizeNames _ _ _ _                                  = undefined -- This case can never be reached.
 
+    -- Helper function validateChar checks whether a character either belongs to the set of reserved special characters of F or is a space.
     validateChar :: Char -> Bool
-    validateChar x = elem x $ [' ', ';', '=', '(', ')', '&', '|', '+', '-', '*', '/', '<', '\n']
+    validateChar x = x `elem` [';', '=', '(', ')', '&', '|', '+', '-', '*', '/', '<', ' ', '\n', '\t', '\r']
