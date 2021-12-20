0ed11cdb70fd7efc6b203e9100d6dac6c070c284
Alexander Richard <richard@cip.ifi.lmu.de>
2021-12-20 12:08:04 +0100

    Added comments.

--- Declarations.hs
+++ Declarations.hs
@@ -29,6 +29,7 @@ module Declarations where
         Times |
         Then               
 
+    -- Adress the fact that boolean values in F are lowercase.
     newtype BoolF = BoolF Bool 
 
     instance Show BoolF where
@@ -53,45 +54,4 @@ module Declarations where
         show RBracket  = ")"
         show Semicolon = ";"
         show Times     = "*"
-        show Then      = "then"
-
-{-
-    type ComparisonOp = Keyword
-    newtype Variable =  Var String
-    data Literal = Bool | Integer
-    data AtomicExpr = Variable | Literal | Expression
-    type Expr8 = [AtomicExpr]
-    type RestExpr7 = [Expr8]
-    data Expr7 = Expr7 Expr8 RestExpr7
-    type Expr6 = Expr7
-    type RestExpr5 = [Expr6]
-    data Expr5 = Expr5 Expr6 RestExpr5
-    data Expr4 = Expr4 Expr5 [(ComparisonOp, Expr5)]
-    type Expr3 = Expr4
-
-    data Definition = Definition [Variable] Expression
-    data Expression = Expr Int
-
-    data SyntaxTree = SyntaxTree [(Token)] deriving (Show)
-    data SyntaxTree = Tree Expression SyntaxTree SyntaxTree | Leaf Expression
-
-    data Expression =
-            Atom AtomExp
-          | Not Expression
-          | Minus Expression
-          | Popen Expression      --open paranthesis
-          | Pclose Expression     --close paranthesis
-          | Add Expression Expression
-          | Subtract Expression Expression
-          | Times Expression Expression
-          | Divide Expression Expression
-          deriving Show
-
-    data Variable = Name String
-    data ComparisonOperator = 
-    data BinSym = And | Or | Equals | Less | Plus | Minus | Times | Divide
-    data AtomExp = VarExp NameToken | NumExp NumberToken | BoolExp BoolToken deriving (Show, Eq)
-    -}
-
-    
-  
+        show Then      = "then" 
