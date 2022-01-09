9a6a6edaa8e62ef43ea7118124f36458eb93dcdd
Alexander Richard <richard@cip.ifi.lmu.de>
2022-01-10 00:21:20 +0100

    Improved access and state functions

--- Store.hs
+++ Store.hs
@@ -3,11 +3,11 @@ module Store where
 
     -- create empty store
     emptyCode, emptyStack, emptyHeap :: Store a
-    emptyCode      = Code []
-    emptyStack     = Stack []
-    emptyHeap      = Heap []
+    emptyCode  = Code []
+    emptyStack = Stack []
+    emptyHeap  = Heap []
 
-    -- push element at the end of a store (not very efficient, but more maybe intuitive for current development)
+    -- Push element on the end of a store. Not very efficient, but more intuitive at the moment.
     push :: Store a -> a -> Store a
     push (Code ccells) elem      = Code (ccells ++ [elem])
     push (Stack scells) elem     = Stack (scells ++ [elem])
@@ -15,30 +15,30 @@ module Store where
 
     -- return depth of store
     depth :: Store a -> Int
-    depth (Code ccells)      = length ccells
-    depth (Stack scells)     = length scells
-    depth (Heap hcells)      = length hcells
+    depth (Code ccells)  = length ccells
+    depth (Stack scells) = length scells
+    depth (Heap hcells)  = length hcells
 
-    -- access n-th element in store
+    -- access element and index ind
     access :: Show a => Store a -> Int -> Either String a
-    access c@(Code ccells) pos      = if (pos <= depth c) && (pos >= 0) && not(null ccells) then return $ ccells !! pos else Left $ "Compile error: " ++ show c ++ " has no index " ++ show pos ++ "."
-    access s@(Stack scells) pos     = if (pos <= depth s) && (pos >= 0) && not(null scells) then return $ scells !! pos else Left $ "Compile error: " ++ show s ++ " has no index " ++ show pos ++ "."
-    access h@(Heap hcells) pos      = if (pos <= depth h) && (pos >= 0) && not(null hcells) then return $ hcells !! pos else Left $ "Compile error: " ++ show h ++ " has no index " ++ show pos ++ "."
+    access c@(Code ccells) ind  = if (ind <= depth c) && (ind >= 0) && not(null ccells) then return $ ccells !! ind else Left $ "Compile error: " ++ show c ++ " has no index " ++ show ind ++ "."
+    access s@(Stack scells) ind = if (ind <= depth s) && (ind >= 0) && not(null scells) then return $ scells !! ind else Left $ "Compile error: " ++ show s ++ " has no index " ++ show ind ++ "."
+    access h@(Heap hcells) ind  = if (ind <= depth h) && (ind >= 0) && not(null hcells) then return $ hcells !! ind else Left $ "Compile error: " ++ show h ++ " has no index " ++ show ind ++ "."
 
     -- reverse store
     reverseStore :: Store a -> Store a
-    reverseStore (Code ccells)      = Code (reverse ccells)
-    reverseStore (Stack scells)     = Stack (reverse scells)
-    reverseStore (Heap hcells)      = Heap (reverse hcells)
+    reverseStore (Code ccells)  = Code (reverse ccells)
+    reverseStore (Stack scells) = Stack (reverse scells)
+    reverseStore (Heap hcells)  = Heap (reverse hcells)
 
-    -- either overwrite n-th element in a store or push element at end of store
+    -- either overwrite element at index ind in a store or push element at end of store
     save :: Show a => Store a -> a -> Int -> Either String (Store a)
-    save c@(Code ccells) ccell pos      = if (pos <= depth c + 1) && (pos >= 0) then return $ Code (take (pos - 1) ccells ++ [ccell] ++ drop pos ccells) else Left $ "Compile error in 'save': " ++ show c ++ " has no index " ++ show pos ++ "."
-    save s@(Stack scells) scell pos     = if (pos <= depth s + 1) && (pos >= 0) then return $ Stack (take (pos - 1) scells ++ [scell] ++ drop pos scells) else Left $ "Compile error in 'save': " ++ show s ++ " has no index " ++ show pos ++ "."
-    save h@(Heap hcells) hcell pos      = if (pos <= depth h + 1) && (pos >= 0) then return $ Heap (take (pos - 1) hcells ++ [hcell] ++ drop pos hcells) else Left $ "Compile error in 'save': " ++ show h ++ " has no index " ++ show pos ++ "."
+    save c@(Code ccells) ccell ind  = if (ind <= depth c) && (ind >= 0) then return $ Code (take ind ccells ++ [ccell] ++ drop (ind + 1) ccells) else Left $ "Compile error in 'save': " ++ show c ++ " has no index " ++ show ind ++ "."
+    save s@(Stack scells) scell ind = if (ind <= depth s) && (ind >= 0) then return $ Stack (take ind scells ++ [scell] ++ drop (ind + 1) scells) else Left $ "Compile error in 'save': " ++ show s ++ " has no index " ++ show ind ++ "."
+    save h@(Heap hcells) hcell ind  = if (ind <= depth h) && (ind >= 0) then return $ Heap (take ind hcells ++ [hcell] ++ drop (ind + 1) hcells) else Left $ "Compile error in 'save': " ++ show h ++ " has no index " ++ show ind ++ "."
 
     {-
-    pop-function not really needed.
+    pop and push functions not really needed at the moment
     pop :: Store a -> Maybe a
     pop (Code (x : xs))      = return x
     pop (Stack (x : xs))     = return x
