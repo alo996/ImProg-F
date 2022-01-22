10da369906671bc718e5bca607059b4796b7b77c
simonhaller <simon@spleen.de>
2022-01-22 17:01:46 +0100

    Store and State Show functions - grafische Darstellung

--- Declarations.hs
+++ Declarations.hs
@@ -3,50 +3,50 @@ module Declarations where
 
     ---------------------------------------- LEXICAL ANALYSIS ----------------------------------------
     {-
-    A token (or symbol) is a sequence of characters with some inherent structure 
+    A token (or symbol) is a sequence of characters with some inherent structure
     (e.g. a reserved F-keyword, a name, a multi-character number).
     -}
-    data Token 
-        = BooleanToken BoolF 
-        | KeywordToken Keyword 
-        | NameToken String 
+    data Token
+        = BooleanToken BoolF
+        | KeywordToken Keyword
+        | NameToken String
         | NumberToken Int
         deriving Eq
 
     {-
-    A keyword is a sequence of characters that is 'reserved' by F. It cannot be used as a name, because F assumes it has a 
+    A keyword is a sequence of characters that is 'reserved' by F. It cannot be used as a name, because F assumes it has a
     predefined meaning (e.g. '+' is meant to be addition).
     -}
-    data Keyword 
-        = And 
-        | Assign 
-        | Divide 
-        | Else 
-        | Equals 
-        | If 
-        | In 
-        | Less 
-        | Let 
-        | LBracket 
-        | Minus 
-        | Not 
-        | Or 
-        | Plus 
-        | RBracket 
-        | Semicolon 
-        | Times 
-        | Then 
-        deriving Eq              
+    data Keyword
+        = And
+        | Assign
+        | Divide
+        | Else
+        | Equals
+        | If
+        | In
+        | Less
+        | Let
+        | LBracket
+        | Minus
+        | Not
+        | Or
+        | Plus
+        | RBracket
+        | Semicolon
+        | Times
+        | Then
+        deriving Eq
 
     -- BoolF adresses the fact that boolean values in F are lowercase.
     newtype BoolF = BoolF Bool deriving Eq
-            
+
     -- We need customized representations of our tokens, depending on their type.
     instance Show Token where
         show (BooleanToken bool)    = show bool
         show (KeywordToken keyword) = show keyword
         show (NameToken name)       = show name
-        show (NumberToken num)      = show num 
+        show (NumberToken num)      = show num
 
     instance Show BoolF where
         show (BoolF True)  = "true"
@@ -70,26 +70,26 @@ module Declarations where
         show RBracket  = ")"
         show Semicolon = ";"
         show Times     = "*"
-        show Then      = "then" 
+        show Then      = "then"
 
 
     ---------------------------------------- SYNTACTICAL ANALYSIS ----------------------------------------
     {-
-    A parser is a parametrized function, that takes a list of tuples. 
+    A parser is a parametrized function, that takes a list of tuples.
     Each tuple contains a token and its line in the source code for error-handling purposes (e.g. [(NameToken f, 1), (NameToken x, 1)]).
     This list of tuples, that represents the tokenized source code, will then be mapped to either an error message (the 'Left' case) if the source code makes no sense,
-    or it will be mapped to a tuple, containing the grammatical equivalent of some tokens, and the rest of the tokenized input (the 'Right' case, 
+    or it will be mapped to a tuple, containing the grammatical equivalent of some tokens, and the rest of the tokenized input (the 'Right' case,
     e.g. "expr8 $ tokenize "y * 8; f x = 3;" --> Right (AtomicExpr (Var "y"),[(*,1),(8,1),(;,1),("f",1),("x",1),(=,1),(3,1),(;,1)]))
     -}
     type Parser a = [(Token, Int)] -> Either String (a, [(Token, Int)])
-    
-    data Def 
+
+    data Def
         = Def Expr [Expr] Expr
         deriving (Eq, Show)
 
     -- A local definition consists of two expressions.
-    data LocalDef 
-        = LocalDef Expr Expr 
+    data LocalDef
+        = LocalDef Expr Expr
         deriving (Eq, Show)
 
     -- A lot of stuff can be an expression, for example '2 + 2'. All possible combinations are summed up in the Expr type.
@@ -97,7 +97,7 @@ module Declarations where
         = Add Expr Expr
         | Func Expr Expr
         | Mult Expr Expr
-        | Div Expr Expr 
+        | Div Expr Expr
         | UnaryMin Expr
         | Equal Expr Expr
         | LessThan Expr Expr
@@ -107,7 +107,7 @@ module Declarations where
         | LetIn [LocalDef] Expr
         | IfThenElse Expr Expr Expr
         | AtomicExpr AtomicExpr
-        deriving Eq 
+        deriving Eq
 
     instance Show Expr where
         show (Add e1 e2)           = show e1 ++ " + " ++ show e2
@@ -125,13 +125,13 @@ module Declarations where
         show (AtomicExpr e)        = show e
 
     -- An atomic expression is the most basic kind of expression. It is either a name, a numeral or a boolean.
-    data AtomicExpr 
+    data AtomicExpr
         = Var String
-        | LitBool BoolF 
-        | LitNum Int 
+        | LitBool BoolF
+        | LitNum Int
         | Expr Expr
-        deriving Eq 
-    
+        deriving Eq
+
     instance Show AtomicExpr where
         show (Var s)                = show s
         show (LitBool (BoolF bool)) = show bool
@@ -142,67 +142,81 @@ module Declarations where
 
     ---------------------------------------- MF-CODE GENERATION AND INTERPRETATION ----------------------------------------
     {-
-    We have four kinds of stores to deal with: code (for instructions), stack (for references), 
+    We have four kinds of stores to deal with: code (for instructions), stack (for references),
     heap (for applications) and global (for definitions of non-local functions).
-    -} 
-    data Store a 
-        = Code [a] 
+    -}
+    data Store a
+        = Code [a]
         | Stack [a]
-        | Heap [a] 
-        deriving (Show, Eq)
+        | Heap [a]
+        deriving Eq
 
-    {- 
+    instance (Show a) => Show (Store a) where
+      show (Code ccells) = "Code: " ++ showCells ccells
+      show (Stack scells) = "Stack: " ++ showCells scells
+      show (Heap hcells) = "Heap: " ++ showCells hcells
+
+    showCells xs = showCells' xs 0 "" where
+      showCells' :: (Show a) => [a] -> Int -> String -> String
+      showCells' (x:xs) ind acc = showCells' xs (ind+1) (acc ++ "\n   " ++ show ind ++ ": " ++ show x)
+      showCells' [] _ acc = acc
+    showCells [] = ""
+
+    {-
     A machine state consists of a program counter, a list of generated instructions, a stack, a heap and a global environment (see Zhu's example).
     Do we also need a stack pointer? If so, why didn't Zhu include one?
-    -} 
-    data State 
-        = State 
+    -}
+    data State
+        = State
         {
             pc :: Int,
             sp :: Int,
             code :: Store Instruction,
             stack :: Store StackCell,
             heap :: Store HeapCell
-        } 
+        }
         | ErrorState String
-        deriving Show
+
+
+    instance Show State where
+        show State {pc=pc, sp=sp, code=Code ccells, stack = stack, heap=heap} = "I:  " ++ show (ccells !! pc) ++ "\nSP: " ++ show sp ++ "\nPC: " ++ show pc ++ "\n" ++ show stack ++ "\n" ++ show heap
 
     -- The instruction type has several constructors, each one indicating some kind of functionality.
-    data Instruction 
-        = Reset 
-        | Pushfun String 
-        | Pushval String Int 
+    data Instruction
+        = Reset
+        | Pushfun String
+        | Pushval String Int
         | Pushparam Int
-        | Makeapp 
-        | Slide Int 
-        | Reduce 
+        | Makeapp
+        | Slide Int
+        | Reduce
         | Return
         | Halt
         | Alloc
-        | UpdateLet Int 
+        | UpdateLet Int
         | SlideLet Int
         | Error String
         deriving Show
 
-    {-A heap is a list of heap cells. 
+    {-A heap is a list of heap cells.
     As the global environment is not changed during runtime, it can be stored at one end of the data store consisting of stack, heap and global environment after compiling.
     -}
-    data HeapCell 
-        = APP Int Int 
+    data HeapCell
+        = APP Int Int
         | VALNum Int
         | VALBool Int
-        | DEF 
+        | DEF
         {
-            fname :: String, 
-            arity :: Int, 
+            fname :: String,
+            arity :: Int,
             caddr :: Int
         }
-        | IND Int 
+        | IND Int
         | PRE Keyword Int
         | UNINITIALIZED
         deriving (Show, Eq)
-    
+
     -- A stack is a list of stack cells.
-    newtype StackCell 
-        = StackCell Int 
+    newtype StackCell
+        = StackCell Int
         deriving (Show, Eq)
