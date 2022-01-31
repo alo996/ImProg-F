7197a287da9b5f4e453f8b324fb2d458315f616e
Alexander Richard <richard@cip.ifi.lmu.de>
2022-01-31 12:59:27 +0100

    Shortened main method

--- Main.hs
+++ Main.hs
@@ -16,7 +16,7 @@ main' input =
     case tokenize input of
         Right toks -> case program toks of
             Right ast  -> case compileProgram (fst ast) of
-                ErrorState error                           -> putStrLn error
-                s@State{pc, sp, code, stack, global, heap} -> putStrLn $ resultToString $ interpret s
+                ErrorState error -> putStrLn error
+                s@State{}        -> putStrLn $ resultToString $ interpret s
             Left error -> putStrLn error
         Left error -> putStrLn error
