b3f233abf2ec4d65e22889010e396bc3555b8579
Alexander Richard <richard@cip.ifi.lmu.de>
2022-01-31 13:00:06 +0100

    Multiple declarations of function names now disallowed

--- Compiler.hs
+++ Compiler.hs
@@ -6,72 +6,59 @@ Description : This module contains all functionality to translate a parsed F pro
 module Compiler where
 
 import Declarations
-    ( Instruction(FuncUpdate, Slide, Unwind, Call, Return, Pushval,
-                  Pushparam, Error, Pushfun, Pushpre, Makeapp, UpdateLet, SlideLet,
-                  Alloc),
-      State(..),
-      HeapCell(DEF, fname, arity),
-      Heap(..),
-      Global(Global),
-      Stack(Stack),
-      Code(Code),
-      AtomicExpr(Var, LitBool, LitNum, Expr),
-      Expr(..),
-      LocalDef(..),
-      Def(..),
-      BoolF(BoolF),
-      Operator(IfOp, PlusOp, TimesOp, DivideOp, EqualsOp, LessOp, AndOp,
-               OrOp, BinaryMinOp, UnaryMinOp, NotOp) )
-import Store ( codeInit )
+import Store
 
 
 -- | Compile a program. 'compileProgram' takes a program (a list of definitions) and returns the initial machine state if successful.
 compileProgram :: [Def] -> State
-compileProgram defs = compileProgram' defs state
+compileProgram defs = compileProgram' defs s
   where
-    state = State {pc = 0, sp = -1, code = codeInit, stack = Stack [], global = Global [], heap = Heap []}
+    s = State {pc = 0, sp = -1, code = codeInit, stack = Stack [], global = Global [], heap = Heap []}
     -- | 'compileProgram'' recursively compiles each definition of the program.
     compileProgram' :: [Def] -> State -> State
-    compileProgram' (d : ds) state = compileProgram' ds (compileDefinition d state)
-    compileProgram' [] state       = lookupMain $ heap state
+    compileProgram' _ (ErrorState error) = ErrorState error
+    compileProgram' (def : defs) s       = compileProgram' defs (compileDefinition def s)
+    compileProgram' [] s                 = lookupMain $ heap s
       where
-        -- | After compiling all definitions, 'lookupMain' checks whether the function main has been specified as required.
+        -- | After compiling all definitions, 'lookupMain' checks whether the function 'main' has been specified as required.
         lookupMain :: Heap -> State
-        lookupMain (Heap (DEF {fname, arity} : hs)) = if fname == "main" && arity == 0 then state else lookupMain (Heap hs)
+        lookupMain (Heap (DEF {fname, arity} : hs)) = if fname == "main" && arity == 0 then s else lookupMain (Heap hs)
         lookupMain _                                = ErrorState "Runtime error: Function main not defined."
 
 -- | Compile a definition. 'compileDefinition' takes the definition to compile and the current machine state and updates its code, global environment and heap.
 compileDefinition :: Def -> State -> State
 compileDefinition 
     (Def (AtomicExpr (Var fname)) es e) 
-    state@State{code = Code ccells, global = Global gcells, heap = Heap hcells} 
-  = state
-    {
-      code = Code (ccells ++ compileExpression e localenv ++ [FuncUpdate len, Slide (len + 1), Unwind, Call, Return]),
-      global = Global (gcells ++ [(fname, length hcells)]),
-      heap = Heap (hcells ++ hcell)
-    }
-      where 
-        localenv = createPos es
-        len = length localenv
-        hcell = [DEF fname (length localenv) (length ccells)]
-compileDefinition def state = ErrorState "Error in 'compileDefinition'"
+    s@State{code = Code ccells, global = Global gcells, heap = Heap hcells} 
+      = if checkDuplicate fname (global s) 
+        then s
+              {
+                code = Code (ccells ++ compileExpression e localenv ++ [FuncUpdate len, Slide (len + 1), Unwind, Call, Return]),
+                global = Global (gcells ++ [(fname, length hcells)]),
+                heap = Heap (hcells ++ hcell)
+              }     
+        else ErrorState $ "Runtime error: Multiple declarations of " ++ show fname ++ "."
+          where 
+            localenv = createPos es
+            len = length localenv
+            hcell = [DEF fname (length localenv) (length ccells)]
+compileDefinition _ _ = ErrorState "Error in 'compileDefinition'"
 
 -- | Compile an expression. 'compileExpression' takes the expression to compile and a local environment and returns a list of MF instructions.
 compileExpression :: Expr -> [(Expr, Int)] -> [Instruction]
 compileExpression e pos = case e of
     AtomicExpr (LitBool (BoolF False)) -> [Pushval "Bool" 0]
     AtomicExpr (LitBool (BoolF True))  -> [Pushval "Bool" 1]
-    AtomicExpr (LitNum num)            -> [Pushval "Int" num]
+    AtomicExpr (LitNum n)              -> [Pushval "Int" n]
     AtomicExpr (Var name)              -> case pos of
         [] -> [Pushfun name]
         _  -> case posInd e pos of
-            Right ind  -> [Pushparam ind]
+            Right n    -> [Pushparam n]
             Left error -> [Error error]
-    AtomicExpr (Expr expr)             -> compileExpression expr pos
+    AtomicExpr (Expr e)                -> compileExpression e pos
     Func (AtomicExpr (Var fname)) e2   -> compileExpression e2 pos ++ [Pushfun fname, Makeapp]
     Func e1 e2                         -> compileExpression e2 pos ++ compileExpression e1 (posInc pos 1) ++ [Makeapp]
-    LetIn localdefs e                  -> compileLocalDefinitions localdefs e pos
+    LetIn ldefs e                      -> compileLocalDefinitions ldefs e pos
     Add e1 e2                          -> compileExpression e2 pos ++ compileExpression e1 (posInc pos 1) ++ [Pushpre PlusOp, Makeapp, Makeapp]
     Mult e1 e2                         -> compileExpression e2 pos ++ compileExpression e1 (posInc pos 1) ++ [Pushpre TimesOp, Makeapp, Makeapp]
     Div e1 e2                          -> compileExpression e2 pos ++ compileExpression e1 (posInc pos 1) ++ [Pushpre DivideOp, Makeapp, Makeapp]
@@ -97,9 +84,9 @@ compileLocalDefinitions ldefs e pos = compileLocalDefinitions' ldefs e (replicat
     It takes the list of local definitions to compile, the expression these local definitions correspond to, the list of already created MF instructions as an accumulator, the local environment of the function, the number of local definitions to compile as argument for the 'SlideLet' instruction after successful compilation and the number of remaining local definitions after each step as argument for the corresponding 'UpdateLet' instruction.
     -}
     compileLocalDefinitions' :: [LocalDef] -> Expr -> [Instruction] -> [(Expr, Int)] -> Int -> Int -> [Instruction]
-    compileLocalDefinitions' ((LocalDef e1 e2) : defs) e ccells pos' n acc =
-        compileLocalDefinitions' defs e (ccells ++ compileExpression e2 pos' ++ [UpdateLet $ acc-1]) pos' n (acc-1)
-    compileLocalDefinitions' [] e ccells pos' n _                          = ccells ++ compileExpression e pos' ++ [SlideLet n]
+    compileLocalDefinitions' ((LocalDef e1 e2) : ldefs) e ccells pos' n acc =
+        compileLocalDefinitions' ldefs e (ccells ++ compileExpression e2 pos' ++ [UpdateLet $ acc-1]) pos' n (acc-1)
+    compileLocalDefinitions' [] e ccells pos' n _                           = ccells ++ compileExpression e pos' ++ [SlideLet n]
 
 -- | Create a local environment for a given list of formal parameters.
 createPos :: [Expr] -> [(Expr, Int)]
@@ -132,3 +119,10 @@ replicate' :: [a] -> Int -> [a]
 replicate' xs n
     | n > 1     = xs ++ replicate' xs (n - 1)
     | otherwise = xs
+
+-- | 'checkDuplicate' returns True if a new function name 'f1' does not already exist in a global environment.
+checkDuplicate :: String -> Global -> Bool 
+checkDuplicate f1 (Global [])             = True 
+checkDuplicate f1 (Global ((f2, _) : gs))
+    | f1 == f2 = False 
+    | otherwise  = checkDuplicate f1 (Global gs)
