81781885ce26ae8315bb01adfe96ceba65438a91
Alexander Richard <richard@cip.ifi.lmu.de>
2022-02-10 12:11:58 +0100

    Reordered

--- Store.hs
+++ Store.hs
@@ -14,21 +14,21 @@ import Declarations
     Code(..))
 
 
--- | Access element at index 'n' in code, stack or heap.
+-- | Access element at index 'n' in a code, stack or heap.
 accessCode :: Code -> Int -> Either String Instruction
 accessCode c@(Code ccells) n
     | n < length ccells && n >= 0 && not (null ccells) = return $ ccells !! n 
     | otherwise                                        = Left $ "Code has no index " ++ show n ++ "."
 
-accessStack :: Stack -> Int -> Either String StackCell
-accessStack s@(Stack scells) n
-    | n < length scells && n >= 0 && not (null scells) = return $ scells !! n 
-    | otherwise                                        = Left $ "Stack has no index " ++ show n ++ "."
-
 accessHeap :: Heap -> Int -> Either String HeapCell
 accessHeap h@(Heap hcells) n 
     | n < length hcells && n >= 0 && not(null hcells) = return $ hcells !! n 
     | otherwise                                       = Left $ "Heap has no index " ++ show n ++ "."
+
+accessStack :: Stack -> Int -> Either String StackCell
+accessStack s@(Stack scells) n
+    | n < length scells && n >= 0 && not (null scells) = return $ scells !! n 
+    | otherwise                                        = Left $ "Stack has no index " ++ show n ++ "."
 
 -- | Initialize code with the set of MF instructions needed before translating function definitions.
 codeInit :: Code
@@ -36,19 +36,19 @@ codeInit =
     Code [Reset, Pushfun "main", Call, Halt, Pushparam 1, Unwind, Call, Pushparam 3, Unwind, Call, Operator 2, OpUpdate, Return, Pushparam 1, Unwind, Call, Operator 3, OpUpdate, Unwind, Call, Return, Pushparam 1, Unwind, Call, Operator 1, OpUpdate, Return]
 
 -- | Push element at the end of a stack or heap.
-pushStack :: Stack -> StackCell -> Stack
-pushStack (Stack scells) elem = Stack $ scells ++ [elem]
-
 pushHeap :: Heap -> HeapCell -> Heap
 pushHeap (Heap hcells) elem = Heap $ hcells ++ [elem]
 
--- | Either overwrite element at index 'n' in a stack or heap, or push element to its end.
-saveStack :: Stack -> StackCell -> Int -> Either String Stack
-saveStack s@(Stack scells) scell n
-    | n <= length scells && n >= 0 = return $ Stack (take n scells ++ [scell] ++ drop (n + 1) scells) 
-    | otherwise                    = Left $ "Stack has no index " ++ show n ++ "."
+pushStack :: Stack -> StackCell -> Stack
+pushStack (Stack scells) elem = Stack $ scells ++ [elem]
 
+-- | Either overwrite element at index 'n' in a stack or heap, or push element to its end.
 saveHeap :: Heap -> HeapCell -> Int -> Either String Heap
 saveHeap h@(Heap hcells) hcell n 
     | n <= length hcells && n >= 0 = return $ Heap (take n hcells ++ [hcell] ++ drop (n + 1) hcells) 
     | otherwise                    = Left $ "Heap has no index " ++ show n ++ "."
+
+saveStack :: Stack -> StackCell -> Int -> Either String Stack
+saveStack s@(Stack scells) scell n
+    | n <= length scells && n >= 0 = return $ Stack (take n scells ++ [scell] ++ drop (n + 1) scells) 
+    | otherwise                    = Left $ "Stack has no index " ++ show n ++ "."
