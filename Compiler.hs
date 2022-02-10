93443b0100373dcbfc928143ac37f7d5f1725b3d
Alexander Richard <richard@cip.ifi.lmu.de>
2022-02-10 12:33:36 +0100

    Adjusted comments

--- Compiler.hs
+++ Compiler.hs
@@ -23,9 +23,6 @@ import Declarations
     Stack(Stack),
     State(..))
 import Store (codeInit)
-import Tokenizer
-import Parser
-import Language.Haskell.Extension (KnownExtension(HereDocuments))
 
 
 ---------------------------------------- F PROGRAM TRANSLATION ----------------------------------------
@@ -46,7 +43,7 @@ compileProgram defs = compileProgram' defs s
             | fname == "main" && arity == 0 = s 
             | fname == "main" && arity > 0  = ErrorState "Runtime error: Function 'main' is not correctly defined." 
             | otherwise                     = lookupMain (Heap hs)
-        lookupMain _                                = ErrorState "Runtime error: Function 'main' is not defined."
+        lookupMain _  = ErrorState "Runtime error: Function 'main' is not defined."
 
 -- | Compile a definition. 'compileDefinition' takes the definition to compile and the current machine state and updates its code, global environment and heap.
 compileDefinition :: Def -> State -> State
@@ -62,9 +59,10 @@ compileDefinition (Def (AtomicExpr (Var fname)) es e) s@State{} = case checkDupl
         localenv = createPos es
         len = length localenv
         hcell = [DEF fname (length localenv) (length ccells)]
-compileDefinition _ _                                            = ErrorState "Error in 'compileDefinition'."
+compileDefinition _ _                                           = ErrorState "Error in 'compileDefinition'."
 
--- | Compile an expression. 'compileExpression' takes the expression to be compiled and a local environment and returns a list of MF instructions.
+{- | Compile an expression. 'compileExpression' takes the expression to compile, a local environment and the already set up global environment of the program and returns a list of MF instructions.
+-}
 compileExpression :: Expr -> [(Expr, Int)] -> Global -> [Instruction]
 compileExpression e pos g = case e of
     AtomicExpr (LitBool (BoolF False))  -> [Pushval "Bool" 0]
@@ -93,7 +91,7 @@ compileExpression e pos g = case e of
     IfThenElse e1 e2 e3                 ->
         compileExpression e3 pos g ++ compileExpression e2 (posInc pos 1) g ++ compileExpression e1 (posInc pos 2) g ++ [Pushpre IfOp, Makeapp, Makeapp, Makeapp]
 
-{- | Compile a local definition. 'compileLocalDefinitions' takes a list of local definitions, the corresponding expression and a local environment. 
+{- | Compile local definitions. 'compileLocalDefinitions' takes a list of local definitions, the expression for which the local definitions have been specified, a local environment and the already set up global environment of the program.
 It returns a list of MF instructions.
 -}
 compileLocalDefinitions :: [LocalDef] -> Expr -> [(Expr, Int)] -> Global ->[Instruction]
@@ -101,7 +99,7 @@ compileLocalDefinitions ldefs e pos s = compileLocalDefinitions' ldefs e (replic
   where
     len = length ldefs
     pos' = createPos' ldefs pos len
-    {- | 'compileLocalDefinitions'' recursively iterates through a list of local definitions and compiles them. 
+    {- | 'compileLocalDefinitions'' recursively iterates through a list of local definitions and compiles each one of them. 
     It takes the list of local definitions to be compiled, the expression these local definitions correspond to, the list of already created MF instructions as an accumulator, the local environment of the function, the number of local definitions to be compiled as argument for the 'SlideLet' instruction after successful compilation and the number of remaining local definitions after each step as argument for the corresponding 'UpdateLet' instruction.
     -}
     compileLocalDefinitions' :: [LocalDef] -> Expr -> [Instruction] -> [(Expr, Int)] -> Global -> Int -> Int -> [Instruction]
@@ -115,8 +113,9 @@ compileLocalDefinitions ldefs e pos s = compileLocalDefinitions' ldefs e (replic
 checkDuplicate :: String -> State -> State
 checkDuplicate f s = checkDuplicate' f s (heap s)
   where
+    -- 'checkDuplicate'' recursively iterates through the heap and checks whether a function 'f' has already been specified.
     checkDuplicate' :: String -> State -> Heap -> State
-    checkDuplicate' f s (Heap (DEF{fname} : hcells)) 
+    checkDuplicate' f s (Heap (DEF{fname} : hcells))
         | f == fname = ErrorState $ "Runtime error in 'checkDuplicate'': Multiple declarations of function '" ++ f ++ "'."
         | otherwise  = checkDuplicate' f s (Heap hcells)
     checkDuplicate' _ s (Heap []) = s
@@ -126,6 +125,7 @@ checkDuplicate f s = checkDuplicate' f s (heap s)
 createGlobal :: [Def] -> State -> State
 createGlobal defs s = createGlobal' defs s 0 
   where
+    -- 'createGlobal'' loops through all definitions of a program and adds (function name, heap position)-pairs to the global environment. 
     createGlobal' :: [Def] -> State -> Int -> State
     createGlobal' 
         ((Def (AtomicExpr (Var fname)) _ _) : defs) 
@@ -143,7 +143,7 @@ createPos' :: [LocalDef] -> [(Expr, Int)] -> Int -> [(Expr, Int)]
 createPos' ldefs pos n = createPos'' ldefs pos' (length ldefs - 2)
   where
     pos' = posInc pos n
-    {- | 'createPos''' takes a list of local definitions, a local environment and an integer.
+    {- | 'createPos''' takes a list of local definitions, a local environment and an index.
     It recursively iterates through the list of local definitions, adding (variable, index)-bindings to the front of the local environment.
     -}
     createPos'' :: [LocalDef] -> [(Expr, Int)] -> Int -> [(Expr, Int)]
@@ -154,7 +154,7 @@ createPos' ldefs pos n = createPos'' ldefs pos' (length ldefs - 2)
 posInc :: [(Expr, Int)] -> Int -> [(Expr, Int)]
 posInc pos n = map (\ (a, b) -> (a, b + n)) pos
 
--- | Get the index of the first occurence of a formal parameter in a given local environment, or look for .. !!!!!!!!! TBD
+-- | Get the index of the first occurence of a formal parameter in a given local environment, or look for a global function with the same name.
 posInd :: Expr -> [(Expr, Int)] -> Global -> [Instruction]
 posInd e@(AtomicExpr (Var name)) pos g@(Global gcells) = case lookup e pos of
     Just n -> [Pushparam n]
